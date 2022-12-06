ff10186426a2b43bf8f27d4f2556e845dd9d2321
Hathilde <mafb@itu.dk>
2022-12-06 17:10:12 +0100

    6/12 - Forsøgt tilføje menubar + opdeling af metoder

--- src/main/java/com/example/wearefucked/NetflixBeta.java
+++ src/main/java/com/example/wearefucked/NetflixBeta.java
@@ -1,32 +1,76 @@
 package com.example.wearefucked;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Stack;
 
 import static javafx.application.Application.launch;
 
 public class NetflixBeta extends Application {
 
+    BorderPane root;
+    StackPane stackPane;
+    FlowPane flowPane;
+
+    ScrollPane scrollPane;
+
     public void start(Stage stage) throws IOException {
         //Layers
-        StackPane root = new StackPane();
-        FlowPane flowPane = new FlowPane();
-        ScrollPane scrollPane = new ScrollPane(flowPane);
-        root.getChildren().add(scrollPane);
+        root = new BorderPane();
+        stackPane = new StackPane();
+        flowPane = new FlowPane();
+        scrollPane = new ScrollPane(flowPane);
+
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        stackPane.getChildren().add(scrollPane);
 
         //Styling
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         flowPane.setPrefWrapLength(1200);
+        flowPane.setHgap(10);
+        flowPane.setVgap(10);
+        //flowPane.setAlignment(Pos.CENTER);
+        flowPane.setStyle("-fx-background-color: black");
+
+        //Stackpane Styling
+        stackPane.setAlignment(Pos.CENTER);
+        flowPane.setCenterShape(true);
+
+        //Scene
+        Scene scene1 = new Scene(root);
+        stage.setScene(scene1);
+        stage.setWidth(500);
+        stage.setHeight(500);
+        stage.setScene(scene1);
+        stage.show();
+
+        Scene scene = new Scene(stackPane, 800, 500);
+        stage.setTitle("PseudoFlix");
+        stage.setScene(scene);
+        stage.show();
+
+        makeMenuBar();              //Kald til makeMenuBar-metoden
+
+
+
+
 
         //Files
         File directory = new File("src/Data/filmplakater/filmplakater");
@@ -40,7 +84,6 @@ public class NetflixBeta extends Application {
             images[i] = new ImageView(fileName[i]);
             String titleName = files[i].getAbsoluteFile().getName();
 
-
             images[i].setUserData(titleName);
             images[i].setId(i + "");
             images[i].setOnMouseClicked((evt) -> {
@@ -48,18 +91,27 @@ public class NetflixBeta extends Application {
                 String targetImage = images[Integer.parseInt(targetPictureSource.getId())].getUserData().toString();
                 System.out.println(evt.getSceneX());
             });
-
-
             flowPane.getChildren().add(images[i]);
         }
 
-        //Scene
-        Scene scene = new Scene(root);
-        stage.setTitle("PseudoFlix");
-        stage.setScene(scene);
-        stage.show();
+        
+
+
+
+
+       // stage.show();
     }
     public static void main(String[] args) {
         launch();
     }
+
+    public void makeMenuBar(){
+        MenuBar menuBar = new MenuBar();
+        Menu Favoritter = new Menu("Favoritter");
+        menuBar.getMenus().add(Favoritter);
+        VBox vBox = new VBox(menuBar);
+        //Scene scene = new Scene(vBox,root);
+        root.setTop(menuBar);
+    }
+
 }
